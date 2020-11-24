b9f7810abbac4724bf593f8303e779efb940d337
HoujieWang <wanghoujie6688@gmail.com>
2020-11-23 19:12:15 -0600

    committed changes vec_fun

--- R/vec_fun.R
+++ R/vec_fun.R
@@ -1,18 +1,20 @@
-#' Vectorize a distance matrix
+#' Vectorize a tree
 #'
-#' @param x A distance matrix between each pair of leaves.
+#' @importFrom ape cophenetic.phylo
+#' @param x A tree object in R as list
 #' @export
 #' @examples
-#' vec_fun(m)
+#' vec_fun(tree)
 #'
 vec_fun<-function(x){
-  # x = cophenetic(x)
-  m<-dim(x)[1]
-  vecTreesVec<-rep(NA,choose(m,2))
-  for(row.num in 1:(m-1)){
-    for(col.num in (row.num+1):m){
-      vecTreesVec[col.num-row.num+(m-1+(m-1-row.num+2))*(row.num-1)/2]<-x[row.num,col.num]
-    }
-  }
-  vecTreesVec
+  x = cophenetic.phylo(x)
+  x[lower.tri(x)]
+  # m<-dim(x)[1]
+  # vecTreesVec<-rep(NA,choose(m,2))
+  # for(row.num in 1:(m-1)){
+  #   for(col.num in (row.num+1):m){
+  #     vecTreesVec[col.num-row.num+(m-1+(m-1-row.num+2))*(row.num-1)/2]<-x[row.num,col.num]
+  #   }
+  # }
+  # vecTreesVec
 }
