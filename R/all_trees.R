f9cde1aceb08d994b4d7495201851aa8e172ba77
HoujieWang <wanghoujie6688@gmail.com>
2021-09-29 14:32:42 -0500

    Bug fixed to pass R CMD check

--- R/all_trees.R
+++ R/all_trees.R
@@ -1,7 +1,7 @@
 ##' Simulated Tree Data with Different Proximity Parameter Value
 ##'
 ##' @docType data
-##' @usage data(all_trees.rda)
+##' @usage data(all_trees)
 ##'
 ##' @format A list of length 12 with each element a sublist containing an
 ##' ape \code{multiPhylo} object with 300 rooted trees on 5 tips and the tree categories.
